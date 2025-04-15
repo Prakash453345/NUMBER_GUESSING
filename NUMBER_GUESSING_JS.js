532a6ba5afafc7b3e63d1163468f393b39912737
Prakash453345 <prakashraj.badugula@gmail.com>
2025-04-15 22:16:39 +0530

    Update NUMBER_GUESSING_JS.js

--- NUMBER_GUESSING_JS.js
+++ NUMBER_GUESSING_JS.js
@@ -11,47 +11,36 @@ let attempts = 0;
 res.addEventListener("click", () => {
     let guess = Number(document.querySelector("#guess").value);
 
-    if (attempts >= limit) {
-        result.innerText = "❌ Sorry, limit exceeded... Try again!";
+    attempts++;
+
+    if (guess === num) {
+        result.innerText = "🎉 Perfect guess!";
+        result.style.backgroundColor = "lightgreen";
         res.disabled = true;
         return;
     }
 
-    attempts++; 
-    if (guess === num) 
-    {
-        result.innerText = "🎉 Perfect guess!";
-        result.style.backgroundColor="lightgreen";
+    if (attempts >= limit) {
+        result.innerText = `❌ You've used all your attempts!..\n The number is : ${num}`;
         res.disabled = true;
-    } 
-    else if (guess > num) {
-        if (guess - num > 15)
-        {
-            result.innerText = `📈 Too High!\nLimits left: ${limit - attempts}`;
-            result.style.backgroundColor="lightcoral";
-        } 
-        else 
-        {
-            result.innerText = `⚠️ Slightly High!\nLimits left: ${limit - attempts}`;
-            result.style.backgroundColor="coral";
-        }
-    } 
-    else {
-        if (num - guess > 15) 
-        {
-            result.innerText = `📉 Too Low!\nLimits left: ${limit - attempts}`;
-            result.style.backgroundColor="lightblue";
-        } 
-        else 
-        {
-            result.innerText = `⚠️ Slightly Low!\nLimits left: ${limit - attempts}`; 
-            result.style.backgroundColor="steelblue";
-        }
+        return;
     }
 
-    
-    if (attempts >= limit) {
-        res.disabled = true;
-        result.innerText = `❌ You've used all your attempts!..\n The number is : ${num}`;
+    if (guess > num) {
+        if (guess - num > 15) {
+            result.innerText = `📈 Too High!\nLimits left: ${limit - attempts}`;
+            result.style.backgroundColor = "lightcoral";
+        } else {
+            result.innerText = `⚠️ Slightly High!\nLimits left: ${limit - attempts}`;
+            result.style.backgroundColor = "coral";
+        }
+    } else {
+        if (num - guess > 15) {
+            result.innerText = `📉 Too Low!\nLimits left: ${limit - attempts}`;
+            result.style.backgroundColor = "lightblue";
+        } else {
+            result.innerText = `⚠️ Slightly Low!\nLimits left: ${limit - attempts}`;
+            result.style.backgroundColor = "steelblue";
+        }
     }
 });
